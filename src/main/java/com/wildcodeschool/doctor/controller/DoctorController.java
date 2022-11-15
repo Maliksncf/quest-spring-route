210f2287799ddeb342f50b74041b8835eb1fd648
Maliksncf <abdelmalik-el-kaddous.bougherara@sncf.fr>
2022-11-15 15:31:38 +0100

    end of task

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -3,15 +3,37 @@ package com.wildcodeschool.doctor.controller;
 import com.wildcodeschool.doctor.model.Doctor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
+import static org.springframework.http.HttpStatus.*;
 
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/")
+    @GetMapping("/doctor/{number}")
     @ResponseBody
-    public Doctor doctor() {
+    public Doctor doctor(@PathVariable int number) {
 
-        return null;
+        switch (number){
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+            case 12:
+                throw new ResponseStatusException(SEE_OTHER);
+            case 13:
+                return new Doctor(number, "Jodie Whittaker");
+            case 42:
+                throw  new ResponseStatusException(NOT_FOUND, "Impossible de récupérer l'incarnation " + number);
+        }
+        return new Doctor();
     }
 }
